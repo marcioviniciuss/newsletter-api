ee82b2483f564187d89e14e61c7e8905b07cafc6
marciovinicius <vmarcio469@gmail.com>
2026-09-06 16:37:37 -0300

    feat: cria o service inicial do projeto para separacao da regra de negocio futura

--- src/main/java/com/marcio/newsletter_api/controllers/SubscriberController.java
+++ src/main/java/com/marcio/newsletter_api/controllers/SubscriberController.java
@@ -1,27 +1,32 @@
 package com.marcio.newsletter_api.controllers;
 
 import com.marcio.newsletter_api.domain.Subscriber;
-import com.marcio.newsletter_api.repositories.SubscriberRepository;
+import com.marcio.newsletter_api.services.SubscriberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("subscribers")
+@RequestMapping("/subscribers")
 public class SubscriberController {
 
-    private final SubscriberRepository subscriberRepository;
+    private final SubscriberService subscriberService;
 
-    public SubscriberController(SubscriberRepository subscriberRepository) {
-        this.subscriberRepository = subscriberRepository;
+    public SubscriberController(SubscriberService subscriberService) {
+        this.subscriberService = subscriberService;
     }
 
     @PostMapping
     public ResponseEntity<Subscriber> saveSubscriber(@RequestBody Subscriber subscriber) {
-        subscriberRepository.save(subscriber);
-        return ResponseEntity.status(HttpStatus.CREATED).body(subscriber);
+        Subscriber responseSubscriber = subscriberService.saveSubscriber(subscriber);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseSubscriber);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Subscriber>> getAllSubscribers() {
+        List<Subscriber> subscribers = subscriberService.findAllSubscribers();
+        return ResponseEntity.status(HttpStatus.OK).body(subscribers);
     }
 }
